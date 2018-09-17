1230530eb4e8113bb4afc7dd0d12aa1f1ca3c951
Fred Neumann <fred.neumann@fau.de>
2018-09-17 23:01:04 +0200

    initial version 1.7.0 for ILIAS 5.3

--- plugin.php
+++ plugin.php
@@ -8,12 +8,12 @@
 $id = "accqst";
  
 // code version; must be changed for all code changes
-$version = "1.5.0";
+$version = "1.7.0";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.2.0";
-$ilias_max_version = "5.2.999";
+$ilias_min_version = "5.3.0";
+$ilias_max_version = "5.3.999";
 
 // add the responsible person for the plugin
 $responsible = "Fred Neumann";
